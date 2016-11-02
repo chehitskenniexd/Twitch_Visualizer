5aca1f2ddc50275056f5d0fc529b5fa95a7a03b7
Kenneth Moy <moykenneth91@gmail.com>
2016-11-01 23:56:45 -0400

    changed the graphs to be next to each other with aligned text

--- Front_End/Components/ChannelComponent.js
+++ Front_End/Components/ChannelComponent.js
@@ -2,8 +2,10 @@
 
 import React from 'react'
 import { convertDateToMDY } from '../../Utilities/utils';
-import { getNumFollowsPerMonth, convertFollowsMonthObjToC3Data,
-    getNumViewsPerMonth, convertViewsVideoObjToC3Data } from '../../Utilities/channelUtils';
+import {
+    getNumFollowsPerMonth, convertFollowsMonthObjToC3Data,
+    getNumViewsPerMonth, convertViewsVideoObjToC3Data
+} from '../../Utilities/channelUtils';
 
 export default class User extends React.Component {
     constructor(props) {
@@ -32,7 +34,7 @@ export default class User extends React.Component {
             this.props.onLoadSearchVideos(channelName);
             return <div></div>;
         }
-        
+
         const followsMonthDataObj = getNumFollowsPerMonth(channelFollows);
         const followerC3Data = convertFollowsMonthObjToC3Data(followsMonthDataObj, 'Number of Follows');
         this.state.followerChartData = c3.generate({
@@ -77,29 +79,34 @@ export default class User extends React.Component {
                     </div>
                 </div>
                 <div className="user-information-cards">
-                    <div className="card" id="follows-info-card">
-                        <div className="card-content">
-                            <span className="card-title activator grey-text text-darken-4">
-                            Follows Per Month for {`${channelFollows.follows.length}`} Most Recent Follows
+                    <div className="row">
+                        <div className="col s12 m8 l6">
+                            <div className="card" id="follows-info-card">
+                                <div className="card-content">
+                                    <span className="card-title activator grey-text text-darken-4">
+                                        Follows Per Month for {`${channelFollows.follows.length}`} Most Recent Follows
                             </span>
-                            <div id="follows-chart"></div>
+                                    <div id="follows-chart"></div>
+                                </div>
+                                <div className="card-reveal">
+                                    <span className="card-title activator grey-text text-darken-4">Followers</span>
+                                    <p>{`Total Followers: ${channelFollows._total}`}</p>
+                                </div>
+                            </div>
                         </div>
-                        <div className="card-reveal">
-                            <span className="card-title activator grey-text text-darken-4">Followers</span>
-                            <p>{`Total Followers: ${channelFollows._total}`}</p>
-                        </div>
-                    </div>
-
-                    <div className="card" id="videos-info-card">
-                        <div className="card-content">
-                            <span className="card-title activator grey-text text-darken-4">
-                            Number of Views: {`${channelVideos.videos.length}`} Most Recent Videos
+                        <div className="col s12 m8 l6">
+                            <div className="card" id="videos-info-card">
+                                <div className="card-content">
+                                    <span className="card-title activator grey-text text-darken-4">
+                                        Number of Views for {`${channelVideos.videos.length}`} Most Recent Videos
                             </span>
-                            <div id="videos-views-chart"></div>
-                        </div>
-                        <div className="card-reveal">
-                            <span className="card-title activator grey-text text-darken-4">Videos</span>
-                            <p>{`Total Videos: ${channelVideos._total}`}</p>
+                                    <div id="videos-views-chart"></div>
+                                </div>
+                                <div className="card-reveal">
+                                    <span className="card-title activator grey-text text-darken-4">Videos</span>
+                                    <p>{`Total Videos: ${channelVideos._total}`}</p>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
