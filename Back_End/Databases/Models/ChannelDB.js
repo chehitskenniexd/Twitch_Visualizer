f725da02197abf6d9498a9b21f00bab2fd36ac75
Kenneth Moy <moykenneth91@gmail.com>
2016-11-10 11:56:25 -0500

    created a channel table beginning backend routing work

---
+++ Back_End/Databases/Models/ChannelDB.js
@@ -0,0 +1,37 @@
+'use strict'
+
+const Sequelize = require('sequelize');
+const db = require('../DBIndex');
+
+const channels = db.define('Channels', {
+    name: {
+        type: Sequelize.STRING,
+        unique: true,
+        validate: {
+            notEmpty: true
+        }
+    },
+    display_name: {
+        type: Sequelize.STRING,
+        validate: {
+            notEmpty: true
+        }
+    },
+    created: {
+        type: Sequelize.DATE
+    },
+    logo: {
+        type: Sequelize.TEXT,
+        defaultValue: null
+    },
+    views: Sequelize.INTEGER,
+    followers: Sequelize.INTEGER,
+    url: {
+        type: Sequelize.TEXT,
+        validate: {
+            notEmpty: true
+        }
+    },
+    partner: Sequelize.BOOLEAN
+} // end channel table creation
+)
