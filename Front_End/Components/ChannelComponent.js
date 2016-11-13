96770b78b8ee0cbda69499484c64e2d1c784dbbe
Kenneth Moy <moykenneth91@gmail.com>
2016-11-12 19:46:38 -0500

    commiting broken changes to refer to old code

--- Front_End/Components/ChannelComponent.js
+++ Front_End/Components/ChannelComponent.js
@@ -4,7 +4,7 @@ import React from 'react'
 import { convertDateToMDY } from '../../Utilities/utils';
 import {
     getNumFollowsPerMonth, convertFollowsMonthObjToC3Data,
-    getNumViewsPerMonth, convertViewsVideoObjToC3Data
+    getNumViewsPerMonth, convertViewsVideoObjToC3Data, createLoadingBar
 } from '../../Utilities/channelUtils';
 
 export default class User extends React.Component {
@@ -17,8 +17,25 @@ export default class User extends React.Component {
     }
 
     componentWillMount() {
+        console.log('will mount');
         if (!this.props.channel) {
             this.props.onLoadChannelInfo(this.props.routeParams.channelName);
+        }
+    }
+
+    componentDidMount() {
+        console.log('did mount');
+        if (this.props.channelFollows) {
+            const followsMonthDataObj = getNumFollowsPerMonth(channelFollows);
+            const followerC3Data = convertFollowsMonthObjToC3Data(followsMonthDataObj, 'Number of Follows');
+            const chartData = c3.generate({
+                bindto: '#follows-chart',
+                data: {
+                    columns: [followerC3Data]
+                }
+            })
+
+            this.setState({ followerChartData: chartData });
         }
     }
 
@@ -28,69 +45,11 @@ export default class User extends React.Component {
         const channelVideos = this.props.channelVideos;
         const channelName = this.props.routeParams.channelName;
 
-        if (!channel) {
-            console.log('hello from no channel!');
+        if (!channel || !channelFollows) {
             return (
-                <div className="row">
-                    <div className="col s12 m12 lg12">
-                        <div className="card">
-                            <div className="loading-bar">
-                                <div className="preloader-wrapper big active">
-                                    <div className="spinner-layer spinner-blue">
-                                        <div className="circle-clipper left">
-                                            <div className="circle"></div>
-                                        </div><div className="gap-patch">
-                                            <div className="circle"></div>
-                                        </div><div className="circle-clipper right">
-                                            <div className="circle"></div>
-                                        </div>
-                                    </div>
-
-                                    <div className="spinner-layer spinner-red">
-                                        <div className="circle-clipper left">
-                                            <div className="circle"></div>
-                                        </div><div className="gap-patch">
-                                            <div className="circle"></div>
-                                        </div><div className="circle-clipper right">
-                                            <div className="circle"></div>
-                                        </div>
-                                    </div>
-
-                                    <div className="spinner-layer spinner-yellow">
-                                        <div className="circle-clipper left">
-                                            <div className="circle"></div>
-                                        </div><div className="gap-patch">
-                                            <div className="circle"></div>
-                                        </div><div className="circle-clipper right">
-                                            <div className="circle"></div>
-                                        </div>
-                                    </div>
-
-                                    <div className="spinner-layer spinner-green">
-                                        <div className="circle-clipper left">
-                                            <div className="circle"></div>
-                                        </div><div className="gap-patch">
-                                            <div className="circle"></div>
-                                        </div><div className="circle-clipper right">
-                                            <div className="circle"></div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+                createLoadingBar()
             );
         }
-
-        const followsMonthDataObj = getNumFollowsPerMonth(channelFollows);
-        const followerC3Data = convertFollowsMonthObjToC3Data(followsMonthDataObj, 'Number of Follows');
-        this.state.followerChartData = c3.generate({
-            bindto: '#follows-chart',
-            data: {
-                columns: [followerC3Data]
-            }
-        })
 
         // const videoViewsDataObj = getNumViewsPerMonth(channelVideos);
         // const videoViewsC3Data = convertViewsVideoObjToC3Data(videoViewsDataObj, 'Number of Views');
@@ -118,7 +77,7 @@ export default class User extends React.Component {
                                         <p>{`View Count: ${channel.views}`}</p>
                                         <p>{`Partner Status: ${channel.partner ? 'Yes' : 'No'}`}</p>
                                         <p>{`Game: ${channel.game}`}</p>
-                                        <p>{`User Since: ${convertDateToMDY(channel.created_at)}`}</p>
+                                        <p>{`User Since: ${convertDateToMDY(channel.created_on)}`}</p>
                                     </div>
                                 </div>
                                 <div className="card-action" id="user-card-actions">
@@ -134,16 +93,17 @@ export default class User extends React.Component {
                             <div className="card" id="follows-info-card">
                                 <div className="card-content">
                                     <span className="card-title activator grey-text text-darken-4">
-                                        Follows Per Month for {`${channelFollows.follows.length}`}Most Recent Follows
+                                        {`Follows Per Month for ${channelFollows.length} Most Recent Follows`}
                             </span>
                                     <div id="follows-chart"></div>
                                 </div>
                                 <div className="card-reveal">
                                     <span className="card-title activator grey-text text-darken-4">Followers</span>
-                                    <p>{`Total Followers: ${channelFollows._total}`}</p>
+                                    <p>{`Total Followers: ${channel.followers}`}</p>
                                 </div>
                             </div>
                         </div>
+                        {/*
                         <div className="col s12 m6 l6">
                             <div className="card" id="videos-info-card">
                                 <div className="card-content">
@@ -157,7 +117,8 @@ export default class User extends React.Component {
                                     <p>{`Total Videos: ${channelVideos._total}`}</p>
                                 </div>
                             </div>
-                        </div>
+                        </div> 
+                        */}
                     </div>
                 </div>
             </div>
--- Utilities/channelUtils.js
+++ Utilities/channelUtils.js
@@ -1,5 +1,7 @@
 'use strict'
 
+import React from 'react';
+
 export function getNumFollowsPerMonth(channelFollows) {
     // This will convert an array of follows into an object with year/month(int)/count
     // Example: output[2016][0] = numUsers;
@@ -56,4 +58,58 @@ export function convertViewsVideoObjToC3Data(dataObject, type) {
         valueArray.push(dataObject[video]);
     }
     return valueArray;
+}
+
+export function createLoadingBar() {
+    return (
+        <div className="row">
+            <div className="col s12 m12 lg12">
+                <div className="card">
+                    <div className="loading-bar">
+                        <div className="preloader-wrapper big active">
+                            <div className="spinner-layer spinner-blue">
+                                <div className="circle-clipper left">
+                                    <div className="circle"></div>
+                                </div><div className="gap-patch">
+                                    <div className="circle"></div>
+                                </div><div className="circle-clipper right">
+                                    <div className="circle"></div>
+                                </div>
+                            </div>
+
+                            <div className="spinner-layer spinner-red">
+                                <div className="circle-clipper left">
+                                    <div className="circle"></div>
+                                </div><div className="gap-patch">
+                                    <div className="circle"></div>
+                                </div><div className="circle-clipper right">
+                                    <div className="circle"></div>
+                                </div>
+                            </div>
+
+                            <div className="spinner-layer spinner-yellow">
+                                <div className="circle-clipper left">
+                                    <div className="circle"></div>
+                                </div><div className="gap-patch">
+                                    <div className="circle"></div>
+                                </div><div className="circle-clipper right">
+                                    <div className="circle"></div>
+                                </div>
+                            </div>
+
+                            <div className="spinner-layer spinner-green">
+                                <div className="circle-clipper left">
+                                    <div className="circle"></div>
+                                </div><div className="gap-patch">
+                                    <div className="circle"></div>
+                                </div><div className="circle-clipper right">
+                                    <div className="circle"></div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 }
